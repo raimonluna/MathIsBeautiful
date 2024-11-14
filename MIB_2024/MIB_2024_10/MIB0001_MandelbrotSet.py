[FIX] Replaced meshgrids by broadcasting

MIB_2024/MIB_2024_10/MIB0001_MandelbrotSet.py
@@ -7,7 +7,8 @@ total_iters  = 100
 xrange       = (-2.1, 0.6)
 yrange       = (-1.35, 1.35)
 
-rec, imc = np.meshgrid(np.linspace(*xrange, shape[0]), np.linspace(*yrange, shape[1]), indexing='ij')
+rec = np.linspace(*xrange, shape[0])[:, None]
+imc = np.linspace(*yrange, shape[1])[None, :]
 c   = rec + 1j * imc
 z   = 0 * c
 mandelbrot = total_iters * np.ones(shape)

MIB_2024/MIB_2024_10/MIB0004_NewtonFractal.py
@@ -7,7 +7,8 @@ total_iters  = 100
 xrange       = (-2, 2)
 yrange       = (-2, 2)
 
-rec, imc = np.meshgrid(np.linspace(*xrange, shape[0]), np.linspace(*yrange, shape[1]), indexing='ij')
+rec = np.linspace(*xrange, shape[0])[:, None]
+imc = np.linspace(*yrange, shape[1])[None, :]
 z   = rec + 1j * imc
 
 iters = - np.ones(shape)

MIB_2024/MIB_2024_10/MIB0006_LogisticMap.py
@@ -10,12 +10,12 @@ yrange      = (-0.05, 1.05)
 
 cmap  = mpl.colors.LinearSegmentedColormap.from_list("", [(0.0, 'wheat'), (0.1, 'orange'), (0.15, 'saddlebrown'), (0.3, 'black'), (1, 'black')])
 
-r, x = np.meshgrid(np.linspace(*xrange, shape[0]), np.linspace(*yrange, shape[1]), indexing='ij')
+r = np.linspace(*xrange, shape[0])
 xn = 0.5 * np.ones(shape[0]) 
 bifurcation = np.ones(shape)
 
 for it in range(total_iters):
-    xn = r[:, 0] * xn * (1 - xn)
+    xn = r * xn * (1 - xn)
     if it > wait_iters:
         idx2 = np.int32(shape[1] * (xn - yrange[0]) / (yrange[1] - yrange[0]))
         bifurcation[range(shape[0]), idx2] += 1

MIB_2024/MIB_2024_10/MIB0007_TrappedKnight.py
@@ -8,8 +8,8 @@ side = 81 # Make it odd!
 cmap_board  = mpl.colors.LinearSegmentedColormap.from_list("", [(0.0, 'mediumblue'), (1, 'darkblue')])
 cmap_knight = mpl.colors.LinearSegmentedColormap.from_list("", [(0.0, 'white'), (0.05, 'saddlebrown'), (0.15, 'saddlebrown'),
                                                                 (0.25, 'darkgreen'), (0.6, 'green'), (1, 'orange')])
-x, y  = np.meshgrid(range(side), range(side), indexing='ij')
-board = (x + y) % 2
+x = np.arange(side)[:, None]
+board = (x + x.T) % 2
 
 ###### Prepare the number spiral ######
 spiral = np.zeros((side, side), dtype = np.int64)

MIB_2024/MIB_2024_11/MIB0013_GoldbachConjecture.py
@@ -12,11 +12,10 @@ is_prime[[0,1]] = False
 for i in range(2, size):
     if is_prime[i]:
         is_prime[2*i::i] = False
-primes = np.arange(size)[is_prime]
+primes = np.arange(size)[is_prime, None]
 
 ##### Do the counting
-a, b = np.meshgrid(primes, primes)
-sums = (a + b)
+sums = primes + primes.T
 sums = np.vstack([sums, np.diag(sums)])
 
 sums = sums[sums <= size]

MIB_2024/MIB_2024_11/MIB0015_BurningShip.py
@@ -8,7 +8,8 @@ total_iters  = 100
 xrange       = (-1.8, -1.7)
 yrange       = (-0.015, 0.085)
 
-rec, imc = np.meshgrid(np.linspace(*xrange, shape[0]), np.linspace(*yrange, shape[1]), indexing='ij')
+rec = np.linspace(*xrange, shape[0])[:, None]
+imc = np.linspace(*yrange, shape[1])[None, :]
 c   = rec + 1j * imc
 z   = 0 * c
 burningship = total_iters * np.ones(shape)